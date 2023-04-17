94c68795d18fc545686399980c03cd2da53db187
DjDeveloperr <djsandhu2005@gmail.com>
2023-04-17 23:28:21 +0530

    x

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -3,20 +3,35 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import config from "./deno.json" assert { type: "json" };
-import * as $0 from "./routes/[name].tsx";
-import * as $1 from "./routes/api/joke.ts";
-import * as $2 from "./routes/index.tsx";
-import * as $$0 from "./islands/Counter.tsx";
+import * as $0 from "./routes/_middleware.ts";
+import * as $1 from "./routes/api/list/[id]/[todo_id].ts";
+import * as $2 from "./routes/api/list/[id]/index.ts";
+import * as $3 from "./routes/api/list/index.ts";
+import * as $4 from "./routes/api/login.ts";
+import * as $5 from "./routes/api/logout.ts";
+import * as $6 from "./routes/api/signup.ts";
+import * as $7 from "./routes/index.tsx";
+import * as $8 from "./routes/list/[id].tsx";
+import * as $9 from "./routes/login.tsx";
+import * as $10 from "./routes/logout.tsx";
+import * as $11 from "./routes/signup.tsx";
 
 const manifest = {
   routes: {
-    "./routes/[name].tsx": $0,
-    "./routes/api/joke.ts": $1,
-    "./routes/index.tsx": $2,
+    "./routes/_middleware.ts": $0,
+    "./routes/api/list/[id]/[todo_id].ts": $1,
+    "./routes/api/list/[id]/index.ts": $2,
+    "./routes/api/list/index.ts": $3,
+    "./routes/api/login.ts": $4,
+    "./routes/api/logout.ts": $5,
+    "./routes/api/signup.ts": $6,
+    "./routes/index.tsx": $7,
+    "./routes/list/[id].tsx": $8,
+    "./routes/login.tsx": $9,
+    "./routes/logout.tsx": $10,
+    "./routes/signup.tsx": $11,
   },
-  islands: {
-    "./islands/Counter.tsx": $$0,
-  },
+  islands: {},
   baseUrl: import.meta.url,
   config,
 };
